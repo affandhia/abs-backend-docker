f694db8619ad79e045ba44744619b7d0b0190281
Affan <affandhia@gmail.com>
2019-06-18 17:12:37 +0700

    Enable port, image, container, and uesr email name configuration.

--- build.sh
+++ build.sh
@@ -1,9 +1,16 @@
+#!bin/bash
+
 # Configuration
-EMAIL='spicervolt@gmail.com'
-GOOGLE_CLIENTID='588654354409-365mn3hbjvam5jp7ggnl7578ssp6otpi.apps.googleusercontent.com'
-BACKEND_PORT=7776
+
+# Usage for setting environment simply: EMAIL="example@email.com" bash build.sh
+
+EMAIL=${EMAIL:='spicervolt@gmail.com'}
+GOOGLE_CLIENTID=${GOOGLE_CLIENTID:='588654354409-365mn3hbjvam5jp7ggnl7578ssp6otpi.apps.googleusercontent.com'}
+BACKEND_PORT=${BACKEND_PORT:=7776}
+BACKEND_CONTAINER_NAME=${BACKEND_CONTAINER_NAME:=abs-backend}
+BACKEND_IMAGE=${BACKEND_IMAGE:=backend}
 # ########### #
 
-docker build -t backend .
-docker rm --force abs-backend
-docker run --name abs-backend -d -e EMAIL=$EMAIL -e GOOGLE_CLIENTID=$GOOGLE_CLIENTID -p $BACKEND_PORT:8081 backend
+docker build -t $BACKEND_IMAGE .
+docker rm --force $BACKEND_CONTAINER_NAME
+docker run --name $BACKEND_CONTAINER_NAME -d -e EMAIL=$EMAIL -e GOOGLE_CLIENTID=$GOOGLE_CLIENTID -p $BACKEND_PORT:8081 $BACKEND_IMAGE
